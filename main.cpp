c9dbd8007a67c5aad8440c0b9f2eac8ed72284cd
WillHalloward <willhalloward@gmail.com>
2019-04-02 22:07:49 +0200

    Minor fixes

--- main.cpp
+++ main.cpp
@@ -1,9 +1,18 @@
 #include <algorithm>
 #include <fstream>
 #include <iostream>
-#include <ostream>
 #include <string>
 #include <vector>
+
+struct Person;
+struct Address;
+size_t find_in_names(const std::vector<Person>& persons, const std::string& name_part);
+std::vector<Person> find_person_from_city(const std::vector<Person>& haystack, const std::string& city);
+std::vector<Person> read_file(const std::string& filename);
+std::string rtrim(const std::string &s);
+std::string caseInsensitive(std::string str);
+std::ostream &operator<<(std::ostream &os, Person &p);
+std::istream &operator>>(std::istream &in, Person &p);
 
 struct Address {
   std::string street;
@@ -16,13 +25,6 @@ struct Person {
   std::string id;
   Address location;
 };
-
-std::ostream &operator<<(std::ostream &os, Person &date);
-std::vector<Person> read_file(std::string filename);
-size_t find_in_names(std::vector<Person> persons, std::string name_part);
-std::vector<Person> find_person_from_city(std::vector<Person> haystack,
-                                          std::string city);
-std::istream &operator>>(std::istream &in, Person &p);
 
 /*size_t find_in_names(std::vector<Person> persons, std::string name_part) {
   int my_count;
@@ -37,33 +39,37 @@ std::istream &operator>>(std::istream &in, Person &p);
   return 0;
 }*/
 
-size_t find_in_names(std::vector<Person> persons, std::string name_part) {
+size_t find_in_names(const std::vector<Person>& persons, const std::string& name_part) {
   int count = 0;
-  transform(name_part.begin(), name_part.end(), name_part.begin(), ::tolower);
-  for (Person c : persons) {
-    std::string str = c.name;
-    transform(str.begin(), str.end(), str.begin(), ::tolower);
-    if (str.find(name_part) != std::string::npos) {
+  for (const Person& c : persons) {
+    if (caseInsensitive(c.name).find(caseInsensitive(name_part)) != std::string::npos) {
       count++;
     }
   }
-  std::cout << count << "\n";
-  return 0;
+  return count;
 }
 
-std::vector<Person> find_person_from_city(std::vector<Person> haystack,
-                                          std::string city) {
-  transform(city.begin(), city.end(), city.begin(), ::tolower);
+std::vector<Person> find_person_from_city(const std::vector<Person>& haystack, const std::string& city) {
   std::vector<Person> vec;
-  for (Person c : haystack) {
-    std::string str = c.location.city;
-    transform(str.begin(), str.end(), str.begin(), ::tolower);
-    if (str == city) {
+  for (const Person& c : haystack) {
+    if (caseInsensitive(c.location.city) == caseInsensitive(city)) {
       vec.push_back(c);
     }
   }
-  for (Person c : vec) {
-    std::cout << c.name << " " << c.location.city << '\n';
+  return vec;
+}
+
+std::vector<Person> read_file(const std::string& filename) {
+  std::vector<Person> vec;
+  std::ifstream names(filename);
+  if (names.fail()) {
+    std::cout << "Fail to open file\n";
+    return vec;
+  }
+  Person temp;
+  while (names.peek() != EOF) {
+    names >> temp;
+    vec.push_back(temp);
   }
   return vec;
 }
@@ -71,6 +77,15 @@ std::vector<Person> find_person_from_city(std::vector<Person> haystack,
 std::string rtrim(const std::string &s) {
   size_t end = s.find_last_not_of(' ');
   return (end == std::string::npos) ? "" : s.substr(0, end + 1);
+}
+
+std::ostream &operator<<(std::ostream &os, Person &p) {
+  os << p.name << '\n';
+  os << p.id << '\n';
+  os << p.location.city << '\n';
+  os << p.location.street << '\n';
+  os << p.location.zip << '\n';
+  return os;
 }
 
 std::istream &operator>>(std::istream &in, Person &p) {
@@ -90,28 +105,10 @@ std::istream &operator>>(std::istream &in, Person &p) {
   return in;
 }
 
-std::vector<Person> read_file(const std::string filename) {
-  std::vector<Person> vec;
-  std::ifstream names(filename);
-  if (names.fail()) {
-    std::cout << "Fail to open file\n";
-    return vec;
-  }
-  Person temp;
-  while (names.peek() != EOF) {
-    names >> temp;
-    vec.push_back(temp);
-  }
-  return vec;
-}
-
-std::ostream &operator<<(std::ostream &os, Person &date) {
-  os << date.name << '\n';
-  os << date.id << '\n';
-  os << date.location.city << '\n';
-  os << date.location.street << '\n';
-  os << date.location.zip << '\n';
-  return os;
+std::string caseInsensitive(std::string str){
+  std::string s = str;
+  transform(s.begin(), s.end(), s.begin(), ::towupper);
+  return s;
 }
 
 /*std::ostream &operator<<(std::ostream &os, Person &date) {
@@ -121,13 +118,13 @@ std::ostream &operator<<(std::ostream &os, Person &date) {
 
 int main() {
   std::vector<Person> vec;
-  vec = read_file("names.txt");
+  vec = read_file("names2.txt");
   /*for (Person c : vec) {
     std::cout << c << '\n';
   }*/
   int mode = 0;
   while (mode < 1 || mode > 3) {
-    std::cout << "1 - Sök del av personnamn.\n"
+    std::cout << "\n1 - Sök del av personnamn.\n"
                  "2 - Sök städer.\n"
                  "3 - Avsluta.\n";
     std::cin >> mode;
@@ -136,7 +133,7 @@ int main() {
       std::string str;
       std::cout << "Vilket namn vill du söka efter? ";
       std::cin >> str;
-      find_in_names(vec, str);
+      std::cout << find_in_names(vec, str);
       mode = 0;
       continue;
     }
@@ -144,7 +141,9 @@ int main() {
       std::string str;
       std::cout << "Vilket stad vill du söka efter? ";
       std::cin >> str;
-      find_person_from_city(vec, str);
+      for (const Person& c : find_person_from_city(vec, str)) {
+        std::cout << c.name << " " << c.location.city << '\n';
+      }
       mode = 0;
       continue;
     }
@@ -152,11 +151,16 @@ int main() {
       std::cout << "Avslutar";
       break;
     default:
-      std::cout
-          << "Vänligen ange ett tal mellan 1 och 3 för att välja alternativ.\n";
+      std::cout << "Vänligen ange ett tal mellan 1 och 3 för att välja alternativ.\n";
     }
     std::cin.clear();
     std::cin.ignore();
   }
   return 0;
 }
+//Questions, Why size_t on find_in_name why not int?
+//Why does inputing 1x, 2x or similar fuck with the menu system?
+/*
+searching for råda does not yeild RÅDA, but searching for skruv yields SKRUV.
+problem with åäö?
+*/
